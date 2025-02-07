344bc9a7e89ebd4ffd9ebb33e460a1c2622d2cc7
heid1.L <165419953+heid1L@users.noreply.github.com>
2025-02-07 11:40:26 +0100

    Test
    test

---
+++ Quizshow/QuizshowV1Server/clienthandler.h
@@ -0,0 +1,14 @@
+#ifndef CLIENTHANDLER_H
+#define CLIENTHANDLER_H
+
+
+class ClientHandler
+{
+public:
+    ClientHandler();
+private:
+
+
+};
+
+#endif // CLIENTHANDLER_H
---
+++ Quizshow/QuizshowV1Server/communication.h
@@ -0,0 +1,13 @@
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+
+class Communication
+{
+public:
+    Communication();
+private:
+    ClientHandler* clientHandler;
+};
+
+#endif // COMMUNICATION_H
